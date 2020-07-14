cbabe192f5e4b103b85fe6740a618cdd59a7655e
edmundo papi <edmundopierre@yahoo.com>
2020-07-14 13:13:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,46 @@
 class Owner
   # code goes here
+#  attr_accessor :name
+  attr_reader :name, :species
+@@all = []
+  def initialize(name)
+    @species = "human"
+    @name = name
+    @@all << self
+  end
+  def say_species
+    "I am a #{@species}."
+  end
+  def self.all
+    @@all
+  end
+  def self.count
+    @@all.size
+  end
+  def self.reset_all
+    @@all.clear
+  end
+  def cats
+    Cat.all.select{|cat|cat.owner ==self}
+  end
+  def dogs
+    Dog.all.select{|dog|dog.owner ==self}
+  end
+  def buy_cat(name)
+    pet = Cat.new(name, self)
+    pet
+  end
+  def buy_dog(name)
+    pet = Dog.new(name, self)
+  end
+
+  def walk_dogs
+      Dog.all.each{|dog|dog.mood= "happy"}
+  end
+  def feed_cats
+   Cat.all.each {|cat|cat.mood ="happy"}
+  end
+  def sell_pets
+  Dog.all.each{|dog|dog.owner.clear}
+  end
 end
